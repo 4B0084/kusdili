06c3fea49e1695b8a09455bd82d039cfbea10b36
4B0084 <59314300+4B0084@users.noreply.github.com>
2020-11-02 14:50:56 +0300

    Update modules.py
    Decode modülü düzeltildi!

--- modules.py
+++ modules.py
@@ -1,7 +1,3 @@
-#-----------------------
-#-------Ejderhan--------
-#-----------------------
-
 sesliharf = ["a","e","ı","i","u","ü","o","ö","A","E","I","İ","U","Ü","O","Ö"] #Sesli harfler tanimlandi
 from time import sleep as sl
 
@@ -25,14 +21,43 @@ def encode(yazi, encodeharf = "g"): #encode icin
     return str(encoded) #String cikis alacagiz
 
 
-###!!!CALISMIYOR!!!###
-def decode(yazi, encodeharf = "g"): #decode icin
-    decoded = ""
+
+def decode(yazi): #decode için GokhanTkr tarafından oluşturuldu
+	delete = []
 
     for i in sesliharf:
         yazi.split(i+encodeharf)
+	b = 0 # for loop element
+	new_list =[]# empty string for rebuilding the word
+	new_str = ""
 
-    return decoded
+	for i in range(len(yazi)): # for loop for detecting the kusdili rules
+
+		if (yazi[i] == 'g'):# search for g letter
+
+			if(yazi[i+1] in sesliharf):
+
+				if(yazi[i-1] in sesliharf):
+
+					delete.append(i-1)
+					delete.append(i)
+					# if the letters are vowels one before and one after from the letter g; store them in a list
+
+
+
+	for a in range(len(yazi)): # loop for deleting sequence
+
+		new_list.append(yazi[a])
+
+	for b in range(len(delete)):
+		new_list[delete[b]] = ""
+
+
+	for c in range(len(new_list)):
+		new_str = new_str + new_list[c]
+
+
+	return new_str
 ###^^^^^^^^^^^^^^^^###
 
 def bruteforce(yazi):
